a5614860eb5abb6de142983c962986af21d05956
dinhvaren <hqhuyzzzz@gmail.com>
2025-05-17 23:34:19 +0700

    first commit

--- src/main/java/com/cybersoft/cozatore/Service/LoginService.java
+++ src/main/java/com/cybersoft/cozatore/Service/LoginService.java
@@ -3,6 +3,7 @@ package com.cybersoft.cozatore.Service;
 import com.cybersoft.cozatore.Entity.UserEntity;
 import com.cybersoft.cozatore.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
